b28f2818f16b256cfbcb19d9420e9424d1d3dcb5
calixlock <calixlock@naver.com>
2023-03-24 10:56:38 +0900

    solved_p120805

--- src/main/java/com/ll/level0/p120804/Main.java
+++ src/main/java/com/ll/level0/p120804/Main.java
@@ -8,8 +8,3 @@ public class Main {
     }
 
 }
-class Solution{
-    public int solution(int n1, int n2){
-        return n1 * n2;
-    }
-}
---
+++ src/main/java/com/ll/level0/p120805/Solution.java
@@ -0,0 +1,8 @@
+package com.ll.level0.p120805;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1/num2;
+        return answer;
+    }
+}
