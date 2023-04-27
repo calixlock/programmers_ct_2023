bfc7c0d2bffc767ecb6abae5c542cda3a2f0a5d4
calixlock <calixlock@naver.com>
2023-04-27 11:10:55 +0900

    solved_181850

---
+++ src/main/java/com/ll/level0/p181850/Solution.java
@@ -0,0 +1,13 @@
+package com.ll.level0.p181850;
+
+public class Solution {
+    public int solution(double flo) {
+        int answer = (int) flo;
+        return answer;
+    }
+//    math class 이용
+    public int solution2(double flo){
+        int answer = (int)Math.floor(flo);
+        return answer;
+    }
+}
