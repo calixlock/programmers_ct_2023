e475863416d0d8acd705a498ed266e17de40636c
calixlock <calixlock@naver.com>
2023-04-27 11:42:19 +0900

    solved_181876

---
+++ src/main/java/com/ll/level0/p181876/Solution.java
@@ -0,0 +1,8 @@
+package com.ll.level0.p181876;
+
+public class Solution {
+    public String solution(String myString) {
+        // String answer = "";
+        return myString.toLowerCase();
+    }
+}
