d02b567ec9c99ad9e08cb0b0f358ee3af7caddd0
calixlock <calixlock@naver.com>
2023-04-29 10:40:54 +0900

    solved_181848

---
+++ src/main/java/com/ll/level0/p181848/Solution.java
@@ -0,0 +1,12 @@
+package com.ll.level0.p181848;
+
+public class Solution {
+    public int solution(String n_str){
+        int answer = Integer.parseInt(n_str);
+        return answer;
+    }
+    public int solution2(String n_str){
+    int answer = Integer.valueOf(n_str);
+    return answer;
+    }
+}
