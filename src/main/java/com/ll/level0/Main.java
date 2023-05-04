d2fb5e7358a3a36664ef387867b9ffcc0bd95dc9
calixlock <calixlock@naver.com>
2023-05-04 11:01:16 +0900

    solved_181835

--- src/main/java/com/ll/level0/Main.java
+++ src/main/java/com/ll/level0/Main.java
@@ -1,5 +1,5 @@
 package com.ll.level0;
-import com.ll.level0.p181884.Solution;
+import com.ll.level0.p181835.Solution;
 
 import java.awt.*;
 import java.util.Arrays;
@@ -10,7 +10,7 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.solution3(new int[]{58, 44, 27, 10, 100},139));
+        System.out.println(sol.solution2(new int[]{58, 44, 27, 10, 100},139));
 //        System.out.println(Arrays.toString(sol.solution4(new int[]{3, 4, 5, 2, 1},2)));
 //        System.out.println(sol.solution4(new int[]{3, 4, 5, 2, 1},2));
     }
--- src/main/java/com/ll/level0/p181835/Solution.java
+++ src/main/java/com/ll/level0/p181835/Solution.java
@@ -1,4 +1,35 @@
 package com.ll.level0.p181835;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Solution {
+    public int[] solution(int[] arr, int k){
+        int[] answer = new int[arr.length];
+        if (k%2 != 0) {
+            for (int i = 0; i < arr.length; i++) {
+                answer[i] = arr[i] * k;
+            }
+        }
+        else{
+            for (int i = 0; i < arr.length; i++) {
+                answer[i] = arr[i] + k;
+            }
+        }
+//        System.out.println(Arrays.toString(answer));
+        return answer;
+    }
+    public int[] solution2(int[] arr, int k){
+        for (int i = 0; i < arr.length; i++) {
+                   arr[i] = ((k&2)==0)? arr[i]+k : arr[i]*k;
+        }
+        return arr;
+    }
+    public int[] solutioin3(int[] arr, int k){
+        if (k%2==0) return IntStream.of(arr).map(i->i+k).toArray();
+        return IntStream.of(arr).map(i->i*k).toArray();
+    }
+    public int[] solutioin4(int[] arr, int k){
+        return Arrays.stream(arr).map(i -> k % 2 == 0? i+k : i*k).toArray();
+    }
 }
