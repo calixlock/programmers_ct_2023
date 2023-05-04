fd76fd6b5fb70f4ea8d958dc242a438e58384ad1
calixlock <calixlock@naver.com>
2023-05-04 13:33:57 +0900

    solved_120822

--- src/main/java/com/ll/level0/Main.java
+++ src/main/java/com/ll/level0/Main.java
@@ -1,5 +1,5 @@
 package com.ll.level0;
-import com.ll.level0.p120821.Solution;
+import com.ll.level0.p120822.Solution;
 
 import java.awt.*;
 import java.util.Arrays;
@@ -12,7 +12,8 @@ public class Main {
         Solution sol = new Solution();
 //        System.out.println(sol.solution(new int[]{1, 2, 3, 4, 5}));
 //        System.out.println(sol.solution(new int[]{58, 44, 27, 10, 100},139));
-        System.out.println(Arrays.toString(sol.solution2(new int[]{3, 4, 5, 2, 1})));
+//        System.out.println(Arrays.toString(sol.solution2(new int[]{3, 4, 5, 2, 1})));
 //        System.out.println(sol.solution4(new int[]{3, 4, 5, 2, 1},2));
+        System.out.println(sol.solution2("jaron"));
     }
 }
---
+++ src/main/java/com/ll/level0/p120822/Solution.java
@@ -0,0 +1,28 @@
+package com.ll.level0.p120822;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public String solution(String my_string){
+        String answer = "";
+        for (int i = 0; i < my_string.length(); i++) {
+            answer += my_string.substring(my_string.length()-i-1,my_string.length()-i);
+        }
+        return answer;
+    }
+    public String solution2(String my_string){
+        String answer = "";
+        String[] arr = my_string.split("");
+        List<String> stringList = Arrays.stream(arr).collect(Collectors.toList());
+        Collections.reverse(stringList);
+//        answer = stringList.stream().map(String::valueOf).collect(Collectors.joining());
+        for (String word:
+             stringList) {
+            answer+=word;
+        }
+        return answer;
+    }
+}
