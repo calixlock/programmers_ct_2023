6fc3361ad4e0c5d8f3b9bded1f80f3494f3cad72
calixlock <calixlock@naver.com>
2023-05-09 06:30:38 +0900

    solved_120826

---
+++ src/main/java/com/ll/level0/p120826/Solution.java
@@ -0,0 +1,9 @@
+package com.ll.level0.p120826;
+
+public class Solution {
+    public String solution(String my_string, String letter) {
+
+        String answer = "";
+        return answer = my_string.replace(letter,"");
+    }
+}
