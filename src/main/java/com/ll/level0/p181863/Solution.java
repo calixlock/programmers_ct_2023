0e62f22ed0c26e1c2aeab087b790a361c0988f92
calixlock <calixlock@naver.com>
2023-05-10 17:14:19 +0900

    solved_181863

---
+++ src/main/java/com/ll/level0/p181863/Solution.java
@@ -0,0 +1,8 @@
+package com.ll.level0.p181863;
+
+public class Solution {
+    public String solution(String rny_string) {
+        String answer = rny_string.replace("m","rn");
+        return answer;
+    }
+}
