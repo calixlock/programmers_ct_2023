abb46d57dc8207c04e3d4db5558642af471ba4b2
calixlock <calixlock@naver.com>
2023-05-11 12:55:29 +0900

    solved_120851

--- src/main/java/com/ll/level0/Main.java
+++ src/main/java/com/ll/level0/Main.java
@@ -1,5 +1,5 @@
 package com.ll.level0;
-import com.ll.level0.p120906.Solution;
+import com.ll.level0.p120851.Solution;
 
 import java.awt.*;
 import java.util.Arrays;
@@ -17,7 +17,10 @@ public class Main {
 //        System.out.println(sol.solution2("i love you"));
 //        System.out.println(sol.solution(4,12));
 //        System.out.println(sol.solution2(new int[]{1, 2, 7, 10, 11}));
-        System.out.println(sol.solution(1234));
-
+        System.out.println(sol.solution2("aAb1B2cC34oOp"));
+//        final String str = "aAb1B2cC34oOp";
+//        str.chars().forEach(System.out::println);
+//        final String str = "aAb1B2cC34oOp";
+//        System.out.println((str.chars().mapToObj(i -> (char) i).filter(Character::isDigit).map(String::valueOf).mapToInt(Integer::valueOf).sum()));
     }
 }
---
+++ src/main/java/com/ll/level0/p120851/Solution.java
@@ -0,0 +1,34 @@
+package com.ll.level0.p120851;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        my_string = my_string.replaceAll("[^0-9]", ""); //문자제거
+        // method_1
+        for (int i = 0; i < my_string.length(); i++) {
+            answer += my_string.charAt(i) - '0'; // 문자 > 숫자 변환 및 연산
+        }
+        // method_2
+//        for(char ch : my_string.toCharArray()) {
+//            answer += Character.getNumericValue(ch);
+//        }
+
+        return answer;
+    }
+    public int solution2(String my_string){
+        int answer = 0;
+        return my_string.chars().mapToObj(i -> (char) i).filter(Character::isDigit).map(String::valueOf).mapToInt(Integer::valueOf).sum();
+    }
+    public int solution3(String my_string) {
+        int answer = 0;
+        for (int i = 0; i < my_string.length(); i++) {
+            // digit를 이용해서 10진수 / 숫자 이외 -1반환
+            if(Character.digit(my_string.charAt(i),10) >= 0){
+                // 문자열을 하나씩 char로 받아내 검사후 char > string > int 변환 및 연산
+                answer += Integer.parseInt(String.valueOf(my_string.charAt(i)));
+            }
+        }return answer;
+    }
+}
