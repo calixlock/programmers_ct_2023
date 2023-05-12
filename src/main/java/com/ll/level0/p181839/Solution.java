dd2cfc4692f40ba6e056e8f2d1856f175eebfe56
calixlock <calixlock@naver.com>
2023-05-12 23:23:27 +0900

    solved_181839

---
+++ src/main/java/com/ll/level0/p181839/Solution.java
@@ -0,0 +1,10 @@
+package com.ll.level0.p181839;
+
+public class Solution {
+    public int solution(int a, int b){
+        int answer =0;
+        if (a%2==1 && b%2==1) return answer = a*a+b*b;
+        else if(a%2==1 || b%2==1) return answer = 2*(a+b);
+        else return a-b>0?a-b:b-a;
+    }
+}
