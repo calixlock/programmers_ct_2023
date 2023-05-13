191b5b3cae239d34a6ea0aa43d6e97ab4cdc49e2
calixlock <calixlock@naver.com>
2023-05-13 23:47:28 +0900

    solved_181828+stream_solved

--- src/main/java/com/ll/level0/p181928/Solution.java
+++ src/main/java/com/ll/level0/p181928/Solution.java
@@ -1,5 +1,9 @@
 package com.ll.level0.p181928;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Solution {
     public int solution(int[] num_list){
         int answer=0;
@@ -13,5 +17,15 @@ public class Solution {
 //        answer = Integer.parseInt(even)+Integer.parseInt(odd); // 11ms
         answer = Integer.valueOf(even)+Integer.valueOf(odd); // 11ms
         return answer;
+    }
+    public int solution2(int[] num_list){
+        return Integer.parseInt(Arrays.stream(num_list)
+                .filter(e -> e%2 == 1)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining())) +
+                Integer.parseInt(Arrays.stream(num_list)
+                        .filter(e -> e%2 != 1)
+                        .mapToObj(String::valueOf)
+                        .collect(Collectors.joining()));
     }
 }
