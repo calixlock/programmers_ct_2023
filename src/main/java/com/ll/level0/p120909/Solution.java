ed0129d059a5aa6ee858a36c2e2e0288f7457e57
calixlock <calixlock@naver.com>
2023-05-16 12:23:27 +0900

    120909_solved

---
+++ src/main/java/com/ll/level0/p120909/Solution.java
@@ -0,0 +1,17 @@
+package com.ll.level0.p120909;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        double sqrt_n = Math.sqrt(n);
+        if(sqrt_n == (int)sqrt_n){
+            return 1;
+        }else return 2;
+    }
+    public int solution2(int n){
+//        if (n % Math.sqrt(n) == 0){
+//            return 1;
+//        }else return 2;
+//    }
+        return n % Math.sqrt(n) == 0 ? 1 : 2;
+}
