29912ed3f451c49da1ac74e2c7f5f99715f03f70
calixlock <calixlock@naver.com>
2023-06-03 23:57:13 +0900

    for(int i=0; i<a.length(); i++){             System.out.println(a.charAt(i));

---
+++ src/main/java/com/ll/level0/p181945/Solution.java
@@ -0,0 +1,17 @@
+package com.ll.level0.p181945;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        for (String str:a.split("")
+             ) {
+            System.out.println(str);
+        }
+//        for(int i=0; i<a.length(); i++){
+//            System.out.println(a.charAt(i));
+//        }
+    }
+}
