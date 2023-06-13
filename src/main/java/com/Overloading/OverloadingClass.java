bddf5d5bfae6bd6674ccea25f8b074b285b85d47
calixlock <calixlock@naver.com>
2023-06-13 15:41:03 +0900

    java_Overloading

---
+++ src/main/java/com/Overloading/OverloadingClass.java
@@ -0,0 +1,22 @@
+package com.Overloading;
+
+public class OverloadingClass {
+    public OverloadingClass() {
+//        getName();
+    }
+    public void getName() {
+        System.out.println("getName() : kim");
+    }
+    public void getName(String name){
+        System.out.println("getName(String name) : "+name);
+    }
+    public void getName(String name,int age){
+        System.out.println("getName(String name,int age) : "+name+" // "+age);
+    }
+
+    public static void main(String[] args) {
+        new OverloadingClass().getName();
+        new OverloadingClass().getName("na");
+        new OverloadingClass().getName("park",20);
+    }
+}
