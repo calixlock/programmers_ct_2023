9bc6f8bc708e6e8535fdeb89822457fe8bdf701d
calixlock <calixlock@naver.com>
2023-06-13 15:32:04 +0900

    java_instance

---
+++ src/main/java/com/instance/CallClass.java
@@ -0,0 +1,10 @@
+package com.instance;
+
+public class CallClass {
+    Contents contents = new Contents();
+    // Contents >> class // new Contents >> instance
+    public static void main(String[] args) {
+        new CallClass();
+
+    }
+}
---
+++ src/main/java/com/instance/Contents.java
@@ -0,0 +1,16 @@
+package com.instance;
+
+public class Contents {
+    private String name;
+    private int age;
+
+    public Contents() {
+        System.out.println("Contents()의 생성자 생성");
+    }
+
+    public Contents(String name, int age) {
+        this.name = name;
+        this.age = age;
+        System.out.println("Contents(String name, int age)의 생성자 생성");
+    }
+}
