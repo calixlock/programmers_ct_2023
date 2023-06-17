20adb008921e18ae147363cddf72eb23ce70579d
calixlock <calixlock@naver.com>
2023-06-17 17:07:27 +0900

    solved add method 181914

--- src/main/java/com/ll/level0/p181914/Solution.java
+++ src/main/java/com/ll/level0/p181914/Solution.java
@@ -1,5 +1,7 @@
 package com.ll.level0.p181914;
 
+import java.util.Arrays;
+
 public class Solution {
     public int solution(String number) {
         int answer = 0;
@@ -12,5 +14,20 @@ public class Solution {
     }
     public int solution2(String number) {
         return number.chars().map(e-> (e-'0')).sum()%9;
+    }
+    public int solution3(String number) { //integer.valueOf 보다 빠르다
+        int answer = 0;
+
+        for(int i=0; i<number.length(); i++) {
+            answer += number.charAt(i) - '0';
+        }
+        answer %= 9;
+        return answer;
+    }
+    public int solution4(String number){
+        int answer = 0;
+        return Arrays.stream(number.split(""))
+                .mapToInt(Integer::parseInt)
+                .sum()%9;
     }
 }
