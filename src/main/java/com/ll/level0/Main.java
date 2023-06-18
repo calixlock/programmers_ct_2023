abf2fbddf8d30a1d9ba7a908027ca83294ac0fce
calixlock <calixlock@naver.com>
2023-06-18 20:58:41 +0900

    solved 181867

--- src/main/java/com/ll/level0/Main.java
+++ src/main/java/com/ll/level0/Main.java
@@ -9,11 +9,17 @@ public class Main {
 //        Solution sol = new Solution();
 //        System.out.println(sol.solution(new solution("43 + 12"));
 //        System.out.println(new Solution().solution(new int[][]{{19, 498, 258, 587}, {63, 93, 7, 754}, {258, 7, 1000, 723}, {587, 754, 723, 81}}));
-//        System.out.println(new Solution().solution("oxoox"));
-        long start = System.currentTimeMillis();
-        System.out.println(start);
-        SimpleDateFormat dateTime = new SimpleDateFormat("yyyy-mm-dd hh-mm-ss");
-        String startTime = dateTime.format(new Date(start));
-        System.out.println(startTime);
+        System.out.println(new Solution().solution("oxooxoxxox"));
+        System.out.println(new Solution().solution("xabcxdefxghi"));
+
+
+
+
+
+//        long start = System.currentTimeMillis();
+//        System.out.println(start);
+//        SimpleDateFormat dateTime = new SimpleDateFormat("yyyy-mm-dd hh-mm-ss");
+//        String startTime = dateTime.format(new Date(start));
+//        System.out.println(startTime);
     }
 }
--- src/main/java/com/ll/level0/p181867/Solution.java
+++ src/main/java/com/ll/level0/p181867/Solution.java
@@ -1,16 +1,26 @@
 package com.ll.level0.p181867;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Solution {
     public int[] solution(String myString) {
-        int[] answer = {};
-        String[] strArr = myString.split("x");
-        answer = new int[strArr.length];
-        for (int i = 0; i < strArr.length; i++) {
-            answer[i] = strArr[i].length();
+        int[] answer = Arrays
+//                .stream(myString.split("x",-1))
+                .stream(myString.split("x",myString.length()))
+                .mapToInt(e->e.length())
+                .toArray();
+        System.out.println("Arrays.toString(answer) = " + Arrays.toString(answer));
+        System.out.println( Arrays.toString(myString.split("x",-1)));
+        return answer;
+    }
+    public int[] solution2(String myString) {
+        String[] arrStr = myString.split("x",-1);
+        int[] answer = new int[arrStr.length];
+        for (int i = 0; i < arrStr.length; i++) {
+            answer[i] = (arrStr[i].length());
         }
-        System.out.println(Arrays.toString(strArr));
         return answer;
     }
 }
